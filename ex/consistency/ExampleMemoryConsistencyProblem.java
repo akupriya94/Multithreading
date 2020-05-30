982b1ac029b1594325ddf0e04cc831ecbe2503e2
akupriya94 <akupriya94@gmail.com>
2020-05-30 10:36:48 +0530

    Update ExampleMemoryConsistencyProblem.java

--- ex/consistency/ExampleMemoryConsistencyProblem.java
+++ ex/consistency/ExampleMemoryConsistencyProblem.java
@@ -2,30 +2,31 @@ package ex.consistency;
 
 public class ExampleMemoryConsistencyProblem {
 
-	public static boolean  flag = false;
+	public static boolean flag = false;
+
 	public static void main(String[] args) throws InterruptedException {
 		Thread thread = new Thread(new Runnable() {
 			public void run() {
-				while(!flag) {
+				while (!flag) {
 
-		           }
-		           System.out.println("Hello World!");
+				}
+				System.out.println("Hello World!");
 
-		           while(flag) {
+				while (flag) {
 
-		           }
-		           System.out.println("Good Bye!");
+				}
+				System.out.println("Good Bye!");
 			}
 		});
-		
+
 		thread.start();
-		
+
 		thread.sleep(1000);
 		System.out.println("Say Hello..");
-        flag = true;
+		flag = true;
 
-        Thread.sleep(1000);
-        System.out.println("Say Bye..");
-        flag = false;
+		Thread.sleep(1000);
+		System.out.println("Say Bye..");
+		flag = false;
 	}
 }
